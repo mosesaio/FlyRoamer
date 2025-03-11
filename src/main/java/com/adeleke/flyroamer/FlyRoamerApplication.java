53c98667b8a50a951dd9393a674a22f3cbc25c04
mosesaio <Ayoaade1@gmail.com>
2025-03-11 06:42:35 -0700

    Initial commit with package and project structure.

---
+++ src/main/java/com/adeleke/flyroamer/FlyRoamerApplication.java
@@ -0,0 +1,13 @@
+package com.adeleke.flyroamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyRoamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyRoamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adeleke/flyroamer/FlyRoamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adeleke.flyroamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyRoamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
